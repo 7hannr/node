360d0a22bb6b7a83dea24f90143540c2c74d3fc6
7hannr <167416215+7hannr@users.noreply.github.com>
2024-06-03 14:40:49 +0900

    Delete ex03 directory

--- ex03/routes/blog.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* 도서검색 */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: '블로그검색',pageName:'blog/search.ejs' });
-});
-
-module.exports = router;

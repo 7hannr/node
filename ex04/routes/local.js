3e648bf3d3cee7ba33b12378d318772a01d5b5f7
7hannr <167416215+7hannr@users.noreply.github.com>
2024-06-03 14:40:58 +0900

    Delete ex04 directory

--- ex04/routes/local.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* 지역검색. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: '지역검색',pageName:'local/search.ejs' });
-});
-
-module.exports = router;
